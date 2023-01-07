834860bc1f4b1e758752e08ae28131008f23dafb
nullparrot <github@weist.dev>
2023-01-06 21:00:16 -0800

    Complet prep 2 Ask for grade and determine letter Determine whether + or -

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,49 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("What grade did you get? (Ex: for '74%' type '74'): ");
+        int grade = int.Parse(Console.ReadLine());
+        int gradeDigit = grade % 10;
+        string gradeLetter;
+        string gradeSign = "";
+        if (grade >= 90)
+        {
+            gradeLetter = "A";
+        }
+        else if (grade >= 80)
+        {
+            gradeLetter = "B";
+
+        }
+        else if (grade >= 70)
+        {
+            gradeLetter = "C";
+
+        }
+        else if (grade >= 60)
+        {
+            gradeLetter = "D";
+
+        }
+        else
+        {
+            gradeLetter = "F";
+        }
+
+
+        if (gradeDigit < 3 && grade > 60){
+            gradeSign = "-";
+        } else if (gradeDigit >= 7 && grade < 97){
+            gradeSign = "+";
+        }
+
+        if (grade >= 70)
+        {
+            Console.WriteLine($"Congrats! You passed your class with an {gradeLetter}{gradeSign}!");
+        }
+        else
+        {
+            Console.WriteLine($"Your grade earned you a {gradeLetter}{gradeSign} in the class.");
+        }
     }
 }
