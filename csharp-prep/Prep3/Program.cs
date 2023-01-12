bd7bb1d886e3fffb2f4c1da786e58cdddc56f153
nullparrot <github@weist.dev>
2023-01-11 18:54:30 -0800

    Complete magic number game

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,48 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        Random rnd = new Random();
+        int magicNumber;
+        int guessNumber;
+        int guesses;
+        bool playing = true;
+        string playingQuest;
+
+        while (playing)
+        {
+            magicNumber = rnd.Next(1, 100);
+            guesses = 0;
+            Console.WriteLine("I'm thinking of a number. Try to guess it!");
+            do
+            {
+                Console.Write("What is your guess? ");
+                guessNumber = int.Parse(Console.ReadLine());
+                guesses += 1;
+                if (guessNumber == magicNumber)
+                {
+                    Console.WriteLine($"You guessed it in {guesses} guess(es)!");
+                    Console.Write("Would you like to keep playing? (Yes/No): ");
+                    playingQuest = Console.ReadLine().ToLower();
+                    if (playingQuest != "yes"){
+                        playing = false;
+                        Console.WriteLine("Thanks for playing!");
+                        Console.WriteLine("Goodbye!");
+                    }
+                }
+                else if (guessNumber < magicNumber)
+                {
+                    Console.WriteLine("Higher");
+                }
+                else if (guessNumber > magicNumber)
+                {
+                    Console.WriteLine("Lower");
+                }
+                else
+                {
+                    Console.WriteLine("Too much magic! Something broke!");
+                }
+            } while (guessNumber != magicNumber);
+        }
+
     }
 }
