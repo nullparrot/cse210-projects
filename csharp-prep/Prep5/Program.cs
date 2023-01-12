937a0b1955c387efea3254898590317471e0e31c
nullparrot <github@weist.dev>
2023-01-11 19:32:07 -0800

    Complate prep 5 collect infor with functions return info with functions

--- csharp-prep/Prep5/Program.cs
+++ csharp-prep/Prep5/Program.cs
@@ -4,6 +4,30 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep5 World!");
+        static void DisplayWelcome(){
+            Console.WriteLine("Welcome to the Program!");
+        }
+        static string PromptUserName(){
+            Console.Write("Please enter your name: ");
+            string name = Console.ReadLine();
+            return name;
+        }
+        static int PromptUserNumber(){
+            Console.Write("Please enter your favorite number: ");
+            int number = int.Parse(Console.ReadLine());
+            return number;
+        }
+        static int SquareNumber(int number){
+            return number*number;
+        }
+        static void DisplayResult(int number, string name){
+            Console.WriteLine($"{name}, the square of your favorite number is {number}");
+        }
+
+        DisplayWelcome();
+        string userName = PromptUserName();
+        int userNumber = PromptUserNumber();
+        int squaredNumber = SquareNumber(userNumber);
+        DisplayResult(squaredNumber,userName);
     }
 }
