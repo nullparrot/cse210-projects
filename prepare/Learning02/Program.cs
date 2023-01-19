8e03d59f1282b83e8069ad27cd0ef2fe9cd070b4
nullparrot <github@weist.dev>
2023-01-18 18:15:40 -0800

    Create Job and resume classes Write display functions for classes Write main function

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -4,6 +4,23 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning02 World!");
+        Job job1 = new Job();
+        job1._company = "McDonalds";
+        job1._jobTitle = "Burger Flipper";
+        job1._startYear = 2008;
+        job1._endYear = 2019;
+        
+        Job job2 = new Job();
+        job2._company = "Sandy Lifeguarding";
+        job2._jobTitle = "Top Runner";
+        job2._startYear = 2019;
+        job2._endYear = 2020;
+
+        Resume myResume = new Resume();
+        myResume._name = "Andrew Weist";
+        myResume._jobs.Add(job1);
+        myResume._jobs.Add(job2);
+
+        myResume.Display();
     }
 }
