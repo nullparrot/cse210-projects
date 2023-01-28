f2afaf29dd2248ddbce2877a17b97301eae6f47c
nullparrot <github@weist.dev>
2023-01-28 07:38:44 -0800

    Test file loading and pprocessing

--- sandbox/Sandbox/Program.cs
+++ sandbox/Sandbox/Program.cs
@@ -1,9 +1,27 @@
 using System;
+using System.IO;
+using System.Text;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Sandbox World!");
+        string path = @"stuff.txt";
+  
+        // Adding below contents to the file
+        //string[] createText = { "GFG is a CS portal." };
+        //File.WriteAllLines(path, createText);
+  
+        // Calling the ReadAllLines() function
+        string[] readText = File.ReadAllLines(path);
+        List<string> mytext = new List<string>();
+        foreach(string s in readText)
+        {
+            mytext.Add(s);
+        }
+
+        foreach(string t in mytext){
+            Console.WriteLine(t);
+        }
     }
 }
