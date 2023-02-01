476cd69869d557aeeec1f88eeb7ca2f2c9ff969a
nullparrot <github@weist.dev>
2023-01-31 18:14:57 -0800

    Make fractions class and test

--- prepare/Learning03/Program.cs
+++ prepare/Learning03/Program.cs
@@ -4,6 +4,16 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning03 World!");
+        Fraction noFraction = new Fraction();
+        Fraction halfFraction = new Fraction(6);
+        Fraction fullFraction = new Fraction(6,7);
+        Console.WriteLine(noFraction.GetDecimalValue());
+        Console.WriteLine(halfFraction.GetFractionString());
+        halfFraction.SetTop(3);
+        halfFraction.SetBottom(6);
+        Console.WriteLine(halfFraction.GetTop());
+        Console.WriteLine(halfFraction.GetBottom());
+        Console.WriteLine(halfFraction.GetFractionString());
+        Console.WriteLine(fullFraction.GetDecimalValue());
     }
 }
