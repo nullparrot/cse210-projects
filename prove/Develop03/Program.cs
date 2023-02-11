5b21513598e0a38591c4cf968665000c6f03df41
nullparrot <github@weist.dev>
2023-02-11 13:29:33 -0800

    Try to stop false possitives on checking progress

--- prove/Develop03/Program.cs
+++ prove/Develop03/Program.cs
@@ -6,26 +6,25 @@ class Program
     {
         Scripture myscripture = new Scripture();
         Words myword = new Words(myscripture.GetScripture());
-        string moving;
+        string moving = "";
         bool doloop = true;
-        string progresscheck;
+        string progresscheck = "";
         string removedword = "";
         do
         {
             Console.Clear();
+            Console.WriteLine(progresscheck);
             Console.WriteLine(myscripture.GetReference());
             Console.WriteLine(myword.GetVerse());
             Console.WriteLine(Environment.NewLine+"We removed the word: '"+removedword+"'");
             Console.WriteLine(Environment.NewLine+"Press enter to continue or 'quit' to finish:");
-            moving = Console.ReadLine();
+            //moving = Console.ReadLine();
             if(moving == "quit"){
                 doloop = false;
             } else{
                 progresscheck = myword.GetVerse();
                 removedword = myword.RemoveWord();
-                removedword = myword.RemoveWord();
-                removedword = myword.RemoveWord();
-                if(progresscheck == myword.GetVerse()){
+                if(String.Compare(progresscheck,myword.GetVerse())<0){
                     doloop = false;
                 }
             }
