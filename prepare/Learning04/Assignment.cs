6a0d0d94949528febebd572276e8b1d7ee577e09
nullparrot <github@weist.dev>
2023-02-15 20:50:53 -0800

    Build out assignment classes

--- prepare/Learning04/Assignment.cs
+++ prepare/Learning04/Assignment.cs
@@ -1,22 +1,48 @@
 public class Assignment{
     private string _studentName;
     private string _topic;
+    public Assignment(string studentName, string topic){
+        _studentName = studentName;
+        _topic = topic;
+    }
     public string GetSummary(){
-        return "";
+        return $"{_studentName} - {_topic}";
+    }
+    public string GetStudentName(){
+        return _studentName;
+    }
+    public string GetTopic(){
+        return _topic;
     }
 }
 
 public class MathAssignment : Assignment{
     private string _textbookSection;
     private string _problems;
+    public MathAssignment(string studentName, string topic,string textbookSection, string problems) : base(studentName,topic){
+        _textbookSection = textbookSection;
+        _problems = problems;
+    }
     public string GetHomeworkList(){
-        return "";
+        return $"Section {_textbookSection} Problems {_problems}";
+    }
+    public string GetTextbookSection(){
+        return _textbookSection;
+    }
+    public string GetProblems(){
+        return _problems;
     }
 }
 
 public class WritingAssignment : Assignment{
     private string _title;
+    public WritingAssignment(string studentName, string topic,string title) : base(studentName,topic){
+        _title = title;
+    }
     public string GetWritingInformation(){
-        return "";
+        return $"{_title} by {GetStudentName()}";
+    }
+    public string GetTitle(){
+        return _title;
     }
 }
