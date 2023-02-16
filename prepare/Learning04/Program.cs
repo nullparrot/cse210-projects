557b8a432f513f7396d842bc89a8a365dd64efc0
nullparrot <github@weist.dev>
2023-02-15 20:54:48 -0800

    Test classes and confirm funtioning

--- prepare/Learning04/Program.cs
+++ prepare/Learning04/Program.cs
@@ -4,6 +4,11 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning04 World!");
+        MathAssignment preCalc = new MathAssignment("Andrew Weist", "Triangles", "2.3", "3-15 Odds");
+        Console.WriteLine(preCalc.GetSummary());
+        Console.WriteLine(preCalc.GetHomeworkList());
+        WritingAssignment thatEssay = new WritingAssignment("Andrew J Weist", "Analysis", "Analysis of 'You Are Not Special'");
+        Console.WriteLine(thatEssay.GetSummary());
+        Console.WriteLine(thatEssay.GetWritingInformation());
     }
 }
