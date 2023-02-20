76b363219cba5627b9a647098ae2a88622d34a40
nullparrot <github@weist.dev>
2023-02-20 13:21:22 -0800

    Create Class Files Build menu class Build base activity class

---
+++ prove/Develop04/activity.cs
@@ -0,0 +1,63 @@
+public class Activity{
+    private int _duration;
+    private long _startTime;
+    private long _endTime;
+    private string _activity;
+    private string _description;
+
+    public Activity(string activity, string description){
+        ActivityWelcome();
+        _activity = activity;
+        _description = description;
+        _startTime = DateTime.Now.Ticks/TimeSpan.TicksPerSecond;
+        _endTime = _startTime+_duration;
+    }
+
+    private void ActivityWelcome(){
+        Console.WriteLine("Thank you for choosing an activity.");
+        Console.Write("How long would you like to participate in the activity (seconds): ");
+        _duration = int.Parse(Console.ReadLine());
+    }
+    public void ActivityGoodbye(){
+        Console.Clear();
+        Console.WriteLine("Thanks for participating. Have a fantastic day!");
+    }
+    public void Pause(int time){
+        long stoptime = DateTime.Now.Ticks/TimeSpan.TicksPerSecond + time;
+        Console.Write("Waiting");
+        int currentLine = Console.GetCursorPosition().Top;
+
+        do
+        {
+            Console.SetCursorPosition(8,currentLine);
+            Console.Write("      ");
+            Thread.Sleep(300);
+            Console.SetCursorPosition(8,currentLine);
+            Console.Write(".     ");
+            Thread.Sleep(234);
+            Console.SetCursorPosition(8,currentLine);
+            Console.Write(". .   ");
+            Thread.Sleep(234);
+            Console.SetCursorPosition(8,currentLine);
+            Console.Write(". . . ");
+            Thread.Sleep(234);
+        } while (stoptime>DateTime.Now.Ticks/TimeSpan.TicksPerSecond);
+    }
+
+    public int GetDuration(){
+        return _duration;
+    }
+
+    public string ChoosePrompt(List<string> prompts){
+        Console.Clear();
+        Console.WriteLine("Please choose a prompt for the provided list:");
+        int tempcount = 1;
+        foreach (string option in prompts)
+        {
+            Console.WriteLine($"{tempcount} - {option}");
+            tempcount++;
+        }
+        Console.Write("Choice (ex. 1): ");
+        return prompts[ int.Parse(Console.ReadLine())-1];
+    }
+}
---
+++ prove/Develop04/breathing.cs
@@ -0,0 +1,7 @@
+public class Breathing : Activity{
+    private string _description = "Breath in and out";
+
+    public Breathing() :base("Breathing Activity",Breathing._description){
+
+    }
+}
