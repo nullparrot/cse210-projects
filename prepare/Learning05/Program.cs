7047c8b7f431a555cdb72a03d2fc03df7e761d5b
nullparrot <github@weist.dev>
2023-03-01 19:02:42 -0800

    Make and test shape classes

--- prepare/Learning05/Program.cs
+++ prepare/Learning05/Program.cs
@@ -4,6 +4,27 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning05 World!");
+
+        // Square peg = new Square(2.5,"Red");
+        // Console.WriteLine($"Square Color: {peg.GetColor()}");
+        // Console.WriteLine($"Square Area: {peg.GetArea()} units^2");
+
+        // Circle hole = new Circle(2.5,"Green");
+        // Console.WriteLine($"Circle Color: {hole.GetColor()}");
+        // Console.WriteLine($"Circle Area: {hole.GetArea()} units^2");
+
+        // Rectangle box = new Rectangle(2.5,5,"Blue");
+        // Console.WriteLine($"Rectangle Color: {box.GetColor()}");
+        // Console.WriteLine($"Rectangle Area: {box.GetArea()} units^2");
+
+
+        List<Shape> myShapes = new List<Shape>();
+        myShapes.Add(new Square(3.4,"Cyan"));
+        myShapes.Add(new Circle(3.0,"Magenta"));
+        myShapes.Add(new Rectangle(4.2,12.1,"Yellow"));
+        foreach(Shape someShape in myShapes){
+            Console.WriteLine($"Shape Color: {someShape.GetColor()}");
+            Console.WriteLine($"Shape Area: {someShape.GetArea()} units^2");
+        }
     }
 }
---
+++ prepare/Learning05/shapes.cs
@@ -0,0 +1,52 @@
+public abstract class Shape{
+    private string _color;
+    
+    public Shape(string color){
+        _color = color;
+    }
+    public abstract double GetArea();
+    public string GetColor(){
+        return _color;
+    }
+    public void SetColor(string color){
+        _color = color;
+    }
+}
+
+public class Square : Shape{
+    private double _side;
+
+    public Square(double side, string color):base(color){
+        _side = side;
+    }
+    public override double GetArea()
+    {
+        return _side*_side;
+    }
+}
+
+public class Circle : Shape{
+    private double _radius;
+
+    public Circle(double radius, string color):base(color){
+        _radius = radius;
+    }
+    public override double GetArea()
+    {
+        return Math.PI*_radius*_radius;
+    }
+}
+
+public class Rectangle : Shape{
+    private double _length;
+    private double _width;
+
+    public Rectangle(double length, double width, string color):base(color){
+        _length = length;
+        _width = width;
+    }
+    public override double GetArea()
+    {
+        return _length*_width;
+    }
+}
