5b60216f8e72aad28824f52a3c6fdcf51a987148
nullparrot <github@weist.dev>
2023-03-10 20:24:28 -0800

    Get minmum done to allow compiling to test simple

--- prove/Develop05/checklist-goal.cs
+++ prove/Develop05/checklist-goal.cs
@@ -2,5 +2,15 @@ public class ChecklistGoal : Goal{
     private int _count;
     private int _progress;
     private int _bonus;
+    public ChecklistGoal(string goal, int value, int count, int progress, int bonus):base(goal,value){
+        _count = count;
+        _progress = progress;
+        _bonus = bonus;
+    }
 
+
+    public override string DisplayGoal()
+    {
+        throw new NotImplementedException();
+    }
 }
--- prove/Develop05/eternal_goal.cs
+++ prove/Develop05/eternal_goal.cs
@@ -1,3 +1,12 @@
 public class EternalGoal : Goal{
     private int _count;
+    public EternalGoal(string goal, int value, int count):base(goal,value){
+        _count = count;
+    }
+
+
+    public override string DisplayGoal()
+    {
+        throw new NotImplementedException();
+    }
 }
