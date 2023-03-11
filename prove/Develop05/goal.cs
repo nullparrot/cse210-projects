b5dc34178bc90cb6dffc85245b7412121740ae13
nullparrot <github@weist.dev>
2023-03-10 20:11:03 -0800

    Build out goals abtracts class

--- prove/Develop05/goal.cs
+++ prove/Develop05/goal.cs
@@ -1,5 +1,31 @@
-public class Goal{
+public abstract class Goal
+{
     private int _value;
     private string _goal;
-    
+    public Goal(string goal, int value)
+    {
+        _goal = goal;
+        _value = value;
+    }
+    public void SetValue(int value)
+    {
+        _value = value;
+    }
+    public int GetValue()
+    {
+        return _value;
+    }
+    public void SetGoal(string goal)
+    {
+        _goal = goal;
+    }
+    public string GetGoal()
+    {
+        return _goal;
+    }
+    public abstract string DisplayGoal();
+    public virtual int GiveReward(){
+        return _value;
+    }
+
 }
