454f1530f540181114d722304cf72f20a7faf2f7
nullparrot <github@weist.dev>
2023-03-10 20:52:17 -0800

    cleanup formating

--- prove/Develop05/checklist-goal.cs
+++ prove/Develop05/checklist-goal.cs
@@ -1,38 +1,47 @@
-public class ChecklistGoal : Goal{
+public class ChecklistGoal : Goal
+{
     private int _count;
     private int _progress;
     private int _bonus;
-    public ChecklistGoal(string goal, int value, int count, int progress, int bonus):base(goal,value){
+    public ChecklistGoal(string goal, int value, int count, int progress, int bonus) : base(goal, value)
+    {
         _count = count;
         _progress = progress;
         _bonus = bonus;
     }
-    public void SetCount(int count){
+    public void SetCount(int count)
+    {
         _count = count;
     }
-    public int GetCount(){
+    public int GetCount()
+    {
         return _count;
     }
-    public void SetProgress(int progress){
+    public void SetProgress(int progress)
+    {
         _progress = progress;
     }
-    public int GetProgress(){
+    public int GetProgress()
+    {
         return _progress;
     }
-    public void SetBonus(int bonus){
+    public void SetBonus(int bonus)
+    {
         _bonus = bonus;
     }
-    public int GetBonus(){
+    public int GetBonus()
+    {
         return _bonus;
     }
     public override string DisplayGoal()
     {
-        if(_progress == _count){
-           return $"[{_progress}/{_count}] - {GetGoal()} - {(_progress*GetValue())+_bonus} Point(s)";
-        } else{
-            return $"[{_progress}/{_count}] - {GetGoal()} - {_progress*GetValue()} Point(s)";
+        if (_progress == _count)
+        {
+            return $"[{_progress}/{_count}] - {GetGoal()} - {(_progress * GetValue()) + _bonus} Point(s)";
         }
-        
-        
+        else
+        {
+            return $"[{_progress}/{_count}] - {GetGoal()} - {_progress * GetValue()} Point(s)";
+        }
     }
 }
