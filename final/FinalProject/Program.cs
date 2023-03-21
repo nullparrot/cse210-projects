e75c44719eaaf4d7cfe823f3651b78dd64d37b83
nullparrot <github@weist.dev>
2023-03-20 19:55:17 -0700

    Build out main menu loop

--- final/FinalProject/Program.cs
+++ final/FinalProject/Program.cs
@@ -4,6 +4,26 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello FinalProject World!");
+        Menu mainMenu = new Menu("Drilling","Tapping","Quit");
+        Menu drillMenu = new Menu("Search Drills","Add Drill","View Drills","Drill Instructions","Main Menu");
+        Menu tapMenu = new Menu("Search Taps","Add Tap","View Taps","Tapping Instructions","Main Menu");
+        bool mainLoop = true;
+        do
+        {
+            Console.Clear();
+            switch(mainMenu.ChooseOne("What would you like to do?")){
+                case "Drilling":
+
+                break;
+                case "Tapping":
+                break;
+                case "Quit":
+                mainLoop = false;
+                break;
+            }
+        
+
+        } while (mainLoop);
+        
     }
 }
