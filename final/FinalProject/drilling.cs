f17d74bc052d1097056a0d21a3a544a9c8a02349
nullparrot <github@weist.dev>
2023-03-27 22:36:23 -0700

    Start tapping and rilling classes

---
+++ final/FinalProject/drilling.cs
@@ -0,0 +1,7 @@
+public class Drilling{
+    private List<Drill> _taps = new List<Drill>();
+    private string _tapFile = "final\FinalProject\drills.csv";
+    public Drilling(){
+        
+    }
+}
---
+++ final/FinalProject/tapping.cs
@@ -0,0 +1,4 @@
+public class Tapping{
+    private List<Tap> _taps = new List<Tap>();
+    private string _tapFile = "final\FinalProject\taps.csv";
+}
